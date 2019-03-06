81b2cbf64f789090f5d1b023aa46c19c09a73d75
mdons <michaeljdonnell@gmail.com>
2019-03-05 19:47:36 -0600

    no errors

--- src/modules/addNumbers.js
+++ src/modules/addNumbers.js
@@ -1,9 +1,13 @@
 import add from "./add";
 
 export function addTen(num) {
-  return add(num,10);
+  return add(num, 10);
 }
 
 export function addFive(num) {
   return add(num, 5);
+}
+
+export function addOne(num) {
+  return add(num, 1);
 }
